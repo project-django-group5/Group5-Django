da881400a3f785441188e842e1bb61b6e5576522
Judith <judithsoundaryaj@gmail.com>
2025-07-10 10:19:54 +0200

    Adding refrained files

--- project_django/skillswap/user_auth/views.py
+++ project_django/skillswap/user_auth/views.py
@@ -1,27 +1,39 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import  LoginForm,RegistrationForm
-#from django.contrib.auth.models import UserAuthenticate
+from .forms import UserForm,RegistrationForm,LoginForm
+from .models import *
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    return render(request,'index.html')
-
-def user_profile(request):
-    return render(request,'profile.html')
+    return render(request,'user_auth/index.html')
 
 def user_signup(request):
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Account created successfully.")
-            return redirect('login')
+    
+        user_form = UserForm(request.POST)
+        profile_form = RegistrationForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+       
+                user = user_form.save(commit=False)
+                user.set_password(user_form.cleaned_data['password']) 
+                user.save()
+                profile = profile_form.save(commit=False)
+                profile.user = user
+                profile.save()
+
+                messages.error(request, "Invalid login details.")
+                return render(request, 'user_auth/login.html')
+        else:
+                user_form = UserForm()
+                profile_form = RegistrationForm()
     else:
         form = RegistrationForm()
-    return render(request, 'templates/register.html', {'form': form})
+    return render(request, 'user_auth/register.html', {'form': form})
+
+
 
 def User_login(request):
     if request.method == 'POST':
@@ -32,7 +44,7 @@ def User_login(request):
         if user:
             if user.is_active:
                login(request,user)
-               return HttpResponseRedirect(reverse('index'))
+               return redirect('index')
             else:
                 return HttpResponse("Account Not Active")
         else:
@@ -43,7 +55,13 @@ def User_login(request):
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
 
+@login_required
 def User_logout(request):
     logout(request)
     messages.info(request, "You have been logged out.")
     return redirect('login')
+
+
+@login_required
+def user_profile(request):
+    return render(request,'user_auth/profile.html')
