7bde563a5bd49392bb8b0c75d697ff1e4694dc45
Elaiyarani S <elaiyarani.s@gmail.com>
2025-07-10 22:49:23 +0200

    Templates added (#11)
    - project level base template added
    - core app to render the home page is added.
    - app level templates added at 'skill app'.

--- project_django/skillswap/user_auth/views.py
+++ project_django/skillswap/user_auth/views.py
@@ -6,8 +6,8 @@ from .models import *
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-def index(request):
-    return render(request,'user_auth/index.html')
+# def index(request):
+#     return render(request,'user_auth/index.html')
 
 def user_signup(request):
     if request.method == 'POST':
@@ -52,7 +52,7 @@ def User_login(request):
             if user is not None:
                 login(request, user)
                 messages.success(request, f"Welcome {username}!")
-                return redirect('index')
+                return redirect('home')
             else:
                 messages.error(request, "Invalid username or password.")
         else:
