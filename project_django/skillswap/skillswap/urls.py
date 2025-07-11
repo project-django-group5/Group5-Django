2a1f8e737ab806530c98234c3653b79d102d21da
Elaiyarani S <elaiyarani.s@gmail.com>
2025-07-11 07:41:27 +0200

    Templates update (#12)

--- project_django/skillswap/skillswap/urls.py
+++ project_django/skillswap/skillswap/urls.py
@@ -23,6 +23,8 @@ from core import views as core_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', core_views.home, name='home'),
+    path('about/', core_views.about, name='about'),
+
     path("", include("user_auth.urls")),
 ]
 
