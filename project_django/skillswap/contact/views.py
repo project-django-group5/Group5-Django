2377d095e903271463fbfeaca56179c1b37811c4
kani <kaniaisu24@gmail.com>
2025-07-13 22:50:56 +0200

    created contact views

--- project_django/skillswap/contact/views.py
+++ project_django/skillswap/contact/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import ContactForm
+from .models import Contact
+
+@login_required
+def contact_form(request, receiver_id):
+    receiver = get_object_or_404(User, pk=receiver_id)
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.sender = request.user
+            contact.receiver = receiver
+            contact.save()
+            return redirect('thank_you')
+    else:
+        form = ContactForm()
+
+    return render(request, 'messaging/contact_form.html', {'form': form, 'receiver': receiver})
+
+def thank_you(request):
+    return render(request, 'messaging/thank_you.html')
+
+
