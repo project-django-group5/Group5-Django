37ddc1afa3f9d16ff50b36afb746454e15655889
kani <kaniaisu24@gmail.com>
2025-07-14 12:18:15 +0200

    changed contact views

--- project_django/skillswap/contact/views.py
+++ project_django/skillswap/contact/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -13,16 +12,23 @@ def contact_form(request, receiver_id):
         form = ContactForm(request.POST)
         if form.is_valid():
             contact = form.save(commit=False)
-            contact.sender = request.user
-            contact.receiver = receiver
+            contact.sender = request.user.username
+            contact.email=request.user.email
+            contact.receiver = receiver.email
             contact.save()
             return redirect('thank_you')
     else:
-        form = ContactForm()
+        form = ContactForm(initial={
+            'sender':request.user.username,
+            'email':request.user.email,
+            'receiver':receiver.email'
+
+        })
 
     return render(request, 'messaging/contact_form.html', {'form': form, 'receiver': receiver})
 
 def thank_you(request):
     return render(request, 'messaging/thank_you.html')
+
 
 
