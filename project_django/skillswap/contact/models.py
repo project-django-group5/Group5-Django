31e1ce32ff827e9897c0662e8879d730826fc8de
Elaiyarani S <elaiyarani.s@gmail.com>
2025-07-14 09:23:33 +0200

    Feature/contact models (#28)
    * Added contact models

--- project_django/skillswap/contact/models.py
+++ project_django/skillswap/contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Contact(models.Model):
+    sender = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField()
+    receiver = models.EmailField()
+    subject = models.CharField(max_length=200, null=False, blank=False)
+    message = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Message from {self.sender} - {self.subject} to {self.receiver}"
