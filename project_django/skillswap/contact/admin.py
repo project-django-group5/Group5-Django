4cf134cc1dc4924f903b28b53fcdda984a94ece2
Sowmya ayarottupura velayudhankutty <sowmyasreesailam@gmail.com>
2025-07-14 09:48:20 +0200

    updated admin.py in contact

--- project_django/skillswap/contact/admin.py
+++ project_django/skillswap/contact/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import contact
+from .models import Contact
 
 # Register your models here.
-admin.site.register(contact)
+admin.site.register(Contact)
