a977a1d8321b95188213525cecc97095d8ff7dcd
Sowmya ayarottupura velayudhankutty <sowmyasreesailam@gmail.com>
2025-07-15 13:31:49 +0200

    fixed conflict

--- project_django/skillswap/review/admin.py
+++ project_django/skillswap/review/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
-
-# Register your models here.
-=======
 from .models import *
 
 
 admin.site.register(Review)
-
-
->>>>>>> 9e8f17c89ae934e7c13571a9db77a0d7a0d9fb2e
