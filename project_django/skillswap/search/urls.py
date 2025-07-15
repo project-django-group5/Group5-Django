f0989f50504c036c0cef649585e6cd26e5759131
Elaiyarani S <elaiyarani.s@gmail.com>
2025-07-15 10:16:41 +0200

    Feature/review templates (#41)

--- project_django/skillswap/search/urls.py
+++ project_django/skillswap/search/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('skill_search/', views.skill_search, name='skill_search'),
+    path('skill_search/',views.skill_search,name='skill_search'),
     path('search/<int:pk>/', views.skill_detail, name='skill_detail'),
 ]
  
