9b64b9d050c80a8b553fbdadd4508d6f86ade41a
Sowmya ayarottupura velayudhankutty <sowmyasreesailam@gmail.com>
2025-07-15 13:07:27 +0200

    fixed the conflict

--- project_django/skillswap/review/forms.py
+++ project_django/skillswap/review/forms.py
@@ -6,11 +6,6 @@ class ReviewForm(forms.ModelForm):
         model = Review
         fields = ['rating', 'comment']
         widgets = {
-<<<<<<< HEAD
-            'rating': forms.RadioSelect(choices=[(i, '') for i in range(1, 5)]),
-=======
             'rating': forms.RadioSelect(choices=[(i, '') for i in range(1, 6)]),
->>>>>>> 9e8f17c89ae934e7c13571a9db77a0d7a0d9fb2e
             'comment': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Write your review...'})
         }
-
