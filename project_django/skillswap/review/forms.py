013284783a875f6c4cb3623ec6a329113f57358a
Judith <judithsoundaryaj@gmail.com>
2025-07-15 11:21:25 +0200

    Adding star form 1 to 6

--- project_django/skillswap/review/forms.py
+++ project_django/skillswap/review/forms.py
@@ -6,7 +6,7 @@ class ReviewForm(forms.ModelForm):
         model = Review
         fields = ['rating', 'comment']
         widgets = {
-            'rating': forms.RadioSelect(choices=[(i, '') for i in range(1, 5)]),
+            'rating': forms.RadioSelect(choices=[(i, '') for i in range(1, 6)]),
             'comment': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Write your review...'})
         }
 
